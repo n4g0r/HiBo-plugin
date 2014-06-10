9675829a10dbea55482d234afc669a65903d3634
Datafridge <feschmi.jena@gmail.com>
2014-06-10 22:09:02 +0200

    clean up

--- hibo/hibo.py
+++ hibo/hibo.py
@@ -33,8 +33,6 @@ class hibo:
     def __init__(self, iface):
         self.iface = iface
         self.gui = Ui_hibo ()
-	#self.gui.setupUi()
-
 
     def initGui(self):  
         self.action = QAction(QIcon("icon.png"), "HiBo", self.iface.mainWindow())
